6c3154a49c75294658275035dbdf9ab63ff615e5
eaapursuit <elishaabochie-ansah@pursuit.org>
2024-01-31 23:02:00 -0500

    added functions that have test calls to practice on writing functions in js

---
+++ functions.js
@@ -0,0 +1,13 @@
+function sayHello(name) {
+  return "Hello " + name;
+}
+
+console.log(sayHello("Alice"));
+console.log(sayHello("Bob"));
+
+function mathProblem(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(mathProblem(4,5))
+console.log(mathProblem(-2,10))
