d9821e61f05c09c86fb558cbe0ea47291f1ecac9
dttrue <danieltorres@pursuit.org>
2024-03-13 11:01:22 -0400

    Daniels Js functions practice

---
+++ functions.js
@@ -0,0 +1,23 @@
+const sayHello = (name) =>{
+    if(name === 'Alice'){
+        return "Hello Alice!"
+    }else if(name === "Bob"){
+        return 'Hello Bob!'
+    }else if(name === undefined){
+        return "Please input name"
+    }else{
+        return "You're not Bob or Alice, but Hello!"
+    }
+}
+
+const addSums = (num1, num2) => {
+    return num1 + num2
+}
+
+console.log(sayHello("Alice"))
+console.log(sayHello('Bob'))
+console.log(sayHello('Dan'))
+console.log(sayHello())
+console.log(addSums(1,5))
+console.log(addSums(10,5))
+console.log(addSums(1,50))
